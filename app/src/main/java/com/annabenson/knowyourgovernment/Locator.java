b312073b9095a203efa0a84548898be0928ace31
annalbenson <annalucybenson@gmail.com>
2018-03-21 16:25:19 -0500

    Start of location code

---
+++ app/src/main/java/com/annabenson/knowyourgovernment/Locator.java
@@ -0,0 +1,127 @@
+package com.annabenson.knowyourgovernment;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by Anna on 3/21/2018.
+ */
+
+public class Locator {
+
+    private static final String TAG = "Locator";
+    private MainActivity owner;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    public Locator(MainActivity activity){
+        owner = activity;
+
+        if(checkPermission()){
+            setUpLocationManager();
+            determineLocation();
+        }
+    }
+
+    public void setUpLocationManager(){
+
+        if(locationManager != null){return;}
+        if(!checkPermission()){ return;}
+
+        // get system's location manager
+        locationManager = (LocationManager) owner.getSystemService(Context.LOCATION_SERVICE);
+
+        // define a listener
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                // called when new location found by network loc provider
+                Log.d(TAG, "onLocationChanged: ");
+                owner.setData(location.getLatitude(), location.getLongitude());
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        // register listener with Logation Manager to receive GPS loc updates
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,1000, 0, locationListener);
+
+    }
+
+    public void shutdown(){
+        locationManager.removeUpdates(locationListener);
+        locationManager = null;
+    }
+
+    // chooses the best location provider Network ==> Passive ==> GPA
+   public void determineLocation(){
+
+        if(!checkPermission()){return;}
+
+        if(locationManager == null){setUpLocationManager();}
+
+       if (locationManager != null) {
+           Location loc = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+           if (loc != null) {
+               owner.setData(loc.getLatitude(), loc.getLongitude());
+               Toast.makeText(owner, "Using " + LocationManager.NETWORK_PROVIDER + " Location provider", Toast.LENGTH_SHORT).show();
+               return;
+           }
+       }
+
+       if (locationManager != null) {
+           Location loc = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+           if (loc != null) {
+               owner.setData(loc.getLatitude(), loc.getLongitude());
+               Toast.makeText(owner, "Using " + LocationManager.PASSIVE_PROVIDER + " Location provider", Toast.LENGTH_SHORT).show();
+               return;
+           }
+       }
+
+       if (locationManager != null) {
+           Location loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+           if (loc != null) {
+               owner.setData(loc.getLatitude(), loc.getLongitude());
+               Toast.makeText(owner, "Using " + LocationManager.GPS_PROVIDER + " Location provider", Toast.LENGTH_SHORT).show();
+               return;
+           }
+       }
+
+       // If you get here, you got no location at all
+       owner.noLocationAvailable();
+       return;
+   }
+
+   private boolean checkPermission(){
+       if (ContextCompat.checkSelfPermission(owner, Manifest.permission.ACCESS_FINE_LOCATION) !=
+               PackageManager.PERMISSION_GRANTED) {
+           ActivityCompat.requestPermissions(owner,
+                   new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 5);
+           return false;
+       }
+       return true;
+   }
+}
--- app/src/main/java/com/annabenson/knowyourgovernment/MainActivity.java
+++ app/src/main/java/com/annabenson/knowyourgovernment/MainActivity.java
@@ -1,7 +1,11 @@
 package com.annabenson.knowyourgovernment;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,8 +21,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import static android.text.InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS;
 
@@ -31,6 +37,9 @@ public class MainActivity extends AppCompatActivity
     private List<Official> officalList = new ArrayList<>();
     private OfficialAdapter officialAdapter;
 
+    private TextView locationText;
+    private Locator locator;
+
     // initial comment 3/19
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +50,11 @@ public class MainActivity extends AppCompatActivity
         officialAdapter = new OfficialAdapter(officalList, this);
         recyclerView.setAdapter(officialAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        locationText = findViewById(R.id.locationDisplay);
+        locator = new Locator(this);
+        //locationText.setText("OK");
+
 
 
         for( int i = 0; i < 5; i++){
@@ -54,6 +68,110 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
+    protected void onDestroy() {
+        locator.shutdown();
+        super.onDestroy();
+    }
+
+
+    /* END OF ON METHODS */
+    /* START OF LOCATION METHODS */
+
+
+    public void setData(double lat, double lon){
+        // gets the results of Locator
+
+        String address = doAddress(lat,lon);
+        locationText.setText(address);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        Log.d(TAG, "onRequestPermissionsResult: CALL: " + permissions.length);
+        Log.d(TAG, "onRequestPermissionsResult: PERM RESULT RECEIVED");
+
+        if (requestCode == 5) {
+            Log.d(TAG, "onRequestPermissionsResult: permissions.length: " + permissions.length);
+            for (int i = 0; i < permissions.length; i++) {
+                if (permissions[i].equals(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                    if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                        Log.d(TAG, "onRequestPermissionsResult: HAS PERM");
+                        locator.setUpLocationManager();
+                        locator.determineLocation();
+                    } else {
+                        Toast.makeText(this, "Location permission was denied - cannot determine address", Toast.LENGTH_LONG).show();
+                        Log.d(TAG, "onRequestPermissionsResult: NO PERM");
+                    }
+                }
+            }
+        }
+        Log.d(TAG, "onRequestPermissionsResult: Exiting onRequestPermissionsResult");
+    }
+
+    private String doAddress(double latitude, double longitude) {
+
+        Log.d(TAG, "doAddress: Lat: " + latitude + ", Lon: " + longitude);
+
+
+
+        List<Address> addresses = null;
+        for (int times = 0; times < 3; times++) {
+            Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+            try {
+                Log.d(TAG, "doAddress: Getting address now");
+
+
+                addresses = geocoder.getFromLocation(latitude, longitude, 1);
+                Log.d(TAG, "doAddress: Num addresses: " + addresses.size());
+
+                StringBuilder sb = new StringBuilder();
+
+                for (Address ad : addresses) {
+                    Log.d(TAG, "doLocation: " + ad);
+
+                    //sb.append("\nAddress\n\n");
+                    /*
+                    Log.d(TAG, "doAddress: " + ad.getMaxAddressLineIndex());
+                    for (int i = 0; i <= ad.getMaxAddressLineIndex(); i++)
+                        sb.append("\t" + ad.getAddressLine(i) + "\t");
+
+                    */
+                    //sb.append("\t" + ad.getCountryName() + " (" + ad.getCountryCode() + ")\n");
+
+                    String s = ad.getAddressLine(0);
+                    int idx = s.indexOf(',');
+                    s = s.substring(idx + 2, s.length());
+                    //int idx2 = s.length() - s.indexOf(',');
+                    //s = s.substring(0,s.length() - idx2);
+                    sb.append(s);
+                }
+
+                return sb.toString();
+            } catch (IOException e) {
+                Log.d(TAG, "doAddress: " + e.getMessage());
+
+            }
+            Toast.makeText(this, "GeoCoder service is slow - please wait", Toast.LENGTH_SHORT).show();
+        }
+        Toast.makeText(this, "GeoCoder service timed out - please try again", Toast.LENGTH_LONG).show();
+        return null;
+    }
+
+
+
+    public void noLocationAvailable() {
+        Toast.makeText(this, "No location providers were available", Toast.LENGTH_LONG).show();
+    }
+
+
+    /* END OF LOCATION METHODS */
+
+    /* START OF CLICK METHODS */
+
+    @Override
     public void onClick(View v){
         Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show();
     }
@@ -64,6 +182,10 @@ public class MainActivity extends AppCompatActivity
 
         return false;
     }
+
+    /* END OF CLICK METHODS */
+
+    /* START OF MENU METHODS */
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
@@ -84,6 +206,10 @@ public class MainActivity extends AppCompatActivity
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    /* END OF MENU METHODS */
+
+    /* START OF DIALOGS */
 
     public void searchDialog(){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -120,4 +246,8 @@ public class MainActivity extends AppCompatActivity
         AlertDialog dialog = builder.create();
         dialog.show();
     }
+
+    /* END OF DIALOGS */
+
+
 }
